23b52806478ec98af2e9e80967db8c5d5ddc6355
Chum4k3r <39815720+Chum4k3r@users.noreply.github.com>
2018-11-03 22:51:38 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setuptools.setup(
     		'Topic :: Software Development :: Build Tools',
 
     		# Pick your license as you wish (should match "license" above)
-     		'License :: OSI Approved :: MIT License',
+     		'License :: OSI Approved :: LGPL License',
 
     		# Specify the Python versions you support here. In particular, ensure
     		# that you indicate whether you support Python 2, Python 3 or both.
