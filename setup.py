7a206adec278ccd112e32a34bbbc5422ed86ea91
João Vitor Gutkoski Paes <39815720+Chum4k3r@users.noreply.github.com>
2018-11-08 21:46:55 -0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 """
                      PyTTa
     Object Oriented Python in Technical Acoustics
@@ -14,59 +14,26 @@
 """
 
 #%%
-import setuptools
+from setuptools import setup
 
-# with open("README.md","r") as fh:
-# 	long_description = fh.read()
-
-setuptools.setup(
-	name = 'PyTTA',
-	version = '0.0.0a1', # ALPHA version release
-	author = ['Marcos Reis',
-	    	'Matheus Lazarin',
-		'João Vitor Paes'],
-	author_email = ['marcos.reis@eac.ufsm.br',
-			'matheus.lazarin@eac.ufsm.br',
-			'joao.paes@eac.ufsm.br'],
-	description = 'Signal processing tools for acoustics and vibrations in python.',
-#	long_description = long_description,
-#	long_description_content_type = 'text/markdown',
-	url = 'http://github.com/pytta',
-	packages = setuptools.find_packages(),
-	zip_safe =  False,
-	license = 'LGPL',
-	install_requires = ['numpy',
-			   'scipy',
-			   'matplotlib',
-			   'sounddevice'],
-	classifiers=[
-    		# How mature is this project? Common values are
-		#   3 - Alpha
-		#   4 - Beta
-    		#   5 - Production/Stable
-    		'Development Status :: 3 - Alpha',
-
-	        # Indicate who your project is intended for
-    		'Intended Audience :: Developers',
-    		'Topic :: Software Development :: Build Tools',
-
-    		# Pick your license as you wish (should match "license" above)
-     		'License :: OSI Approved :: LGPL License',
-
-    		# Specify the Python versions you support here. In particular, ensure
-    		# that you indicate whether you support Python 2, Python 3 or both.
-    		'Programming Language :: Python :: 3.4',
-    		'Programming Language :: Python :: 3.5',
-    		'Programming Language :: Python :: 3.6',
-	    	'Programming Language :: Python :: 3.7']
-
-#    'package_data': {  # apenas para arquivos com outras extensões (.c, .h, .exe, etc)
-#        'pytta': [
+settings = {
+    'name': 'PyTTa',
+    'version': '0.0.0b',
+    'description': 'Signal processing tools for acoustics and vibrations in python.',
+    'url': 'http://github.com/pytta',
+    'author': 'Marcos Reis, Matheus Lazarin, João Vitor Paes',
+    'packages': ['pytta'],
+    'zip_safe': False,
+    'author_email': 'joao.paes@eac.ufsm.br',
+    'license': 'LGPL',
+    'install_requires': ['numpy','scipy','sounddevice','pyfilterbank'],
+#    'package_data': {
+#        'pyfilterbank': [
 #            'sosfilt.c',
 #            'sosfilt64.dll',
 #            'sosfilt32.dll',
 #            'sosfilt.so'
 #        ]
 #    }
-
-)
+}
+setup(**settings)
