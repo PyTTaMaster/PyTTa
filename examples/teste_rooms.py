[FIX] Print on calculation

examples/teste_rooms.py
@@ -12,11 +12,11 @@ from scipy.io import loadmat
 from matplotlib import pyplot as plt
 
 
-f = loadmat('RIS/RI_mono_3.mat')
+f = loadmat('RIS/RI_mono_2.mat')
 
 
-mySignal = pytta.SignalObj(f['MonoRIS3_time'], 'time', 44100)
+mySignal = pytta.SignalObj(f['MonoRIS2_time'], 'time', 44100)
 
 
-myOut = pytta.rooms.RT('EDT', mySignal, 3)
+myOut = pytta.rooms.RT('20', mySignal, 3)
 