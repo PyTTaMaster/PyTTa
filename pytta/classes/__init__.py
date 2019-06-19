228049167a8bba1e14359afc53c864c9086979b8
Chum4k3r <joao.paes@eac.ufsm.br>
2019-06-19 15:24:47 -0300

    [INTERNAL] Moved filter.py to classes package, added analysis.py

---
+++ pytta/classes/__init__.py
@@ -0,0 +1,9 @@
+from signal import SignalObj, ImpulsiveResponse
+from measurement import RecMeasure, PlayRecMeasure, FRFMeasure
+from streaming import Streaming
+from filter import OctFilter, weighting
+
+__all__ = ['SignalObj', 'ImpulsiveResponse',
+           'RecMeasure', 'PlayRecMeasure', 'FRFMeasure',
+           'Streaming',
+           'OctFilter', 'weighting']
