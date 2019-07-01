b13b3b13350d84a6d3c27f111b2919f1c85950b1
Chum4k3r <joao.paes@eac.ufsm.br>
2019-07-01 14:21:12 -0300

    [FIX] __print_max_level rename to _print_max_levl

--- pytta/classes/measurement.py
+++ pytta/classes/measurement.py
@@ -214,7 +214,7 @@ class RecMeasure(Measurement):
         recording.freqMin, recording.freqMax\
             = (self.freqMin, self.freqMax)
         recording.comment = 'SignalObj from a Rec measurement'
-        __print_max_level(recording, kind='input')
+        _print_max_level(recording, kind='input')
         return recording
 
 
@@ -303,8 +303,8 @@ class PlayRecMeasure(Measurement):
         recording.freqMin, recording.freqMax\
             = (self.freqMin, self.freqMax)
         recording.comment = 'SignalObj from a PlayRec measurement'
-        __print_max_level(self.excitation, kind='output')
-        __print_max_level(recording, kind='input')
+        _print_max_level(self.excitation, kind='output')
+        _print_max_level(recording, kind='input')
         return recording
 
     def _to_dict(self):
@@ -461,7 +461,7 @@ class FRFMeasure(PlayRecMeasure):
 
 
 # Sub functions
-def __print_max_level(sigObj, kind):
+def _print_max_level(sigObj, kind):
     if kind == 'output':
         for chIndex in range(sigObj.num_channels()):
             print('max output level (excitation) on channel ['
