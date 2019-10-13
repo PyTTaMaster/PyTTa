9a592f71115b06bd03a246c9e3ab23e6cc783d19
Matheus Lazarin <mtslazarin@gmail.com>
2019-10-13 18:24:57 -0300

    [DOC] some pytta.roomir doc

--- pytta/examples/roomir_measurement.py
+++ pytta/examples/roomir_measurement.py
@@ -8,9 +8,13 @@ Created on Sun Jun 23 15:08:25 2019
 # %% Initializating
 import pytta
 from pytta import roomir as rmr
-from pytta.classes import lju3ei1050
-# %%
-path = 'pytta/examples/'
+import os
+# from pytta.classes import lju3ei1050 # Comunicação c/ o LabJack U3 + EI1050
+
+# %% Muda o current working directory do Python para a pasta onde este script
+# se encontra
+cwd = os.path.dirname(__file__) # Pega a pasta de trabalho atual
+os.chdir(cwd)
 
 # %%
 # Cria objeto para stream de dados com o LabJack U3 com o sensor
@@ -18,7 +22,7 @@ path = 'pytta/examples/'
 # tempHumid = lju3ei1050.main()
 tempHumid = None  # Para testes com LabJack offline
 
-# %% Carrega sinais de excitação
+# %% Carrega sinais de excitação e cria dicionário para o setup da medição
 excitationSignals = {}
 excitationSignals['varredura'] = pytta.generate.sweep(
         # Geração do sweep (também pode ser carregado projeto prévio)
@@ -31,22 +35,26 @@ excitationSignals['varredura'] = pytta.generate.sweep(
         windowing='hann')
 # Carregando sinal de música
 excitationSignals['musica'] = pytta.read_wav(
-        path + 'audio/Piano Over the rainbow Mic2 SHORT_edited.wav')
+        'audio/Piano Over the rainbow Mic2 SHORT_edited.wav')
 # Carregando sinal de fala
 excitationSignals['fala'] = pytta.read_wav(
-        path + 'audio/Voice Sabine Short_edited.WAV')
+        'audio/Voice Sabine Short_edited.WAV')
 
-# %% Cria novo Setup de Medição
+# %% Caso já tenha uma medição em curso, carregue o MeasurementSetup e 
+# MeasurementData
+MS, D = rmr.med_load('med-teste')
+
+# %% Cria novo setup de medição e inicializa objeto de dados, que gerencia o
+# MeasurementSetup e os dados da medição em disco
 MS = rmr.MeasurementSetup(name='med-teste',  # Nome da medição
                           samplingRate=44100,  # [Hz]
                           # Sintaxe : device = [<in>,<out>] ou <in/out>
                           # Utilize pytta.list_devices() para listar
                           # os dispositivos do seu computador.
                           #   device=[0, 1],  # PC laza
-                            device=4,  # Saffire Pro 40 laza
+                          device=4,  # Saffire Pro 40 laza
                           # device=[1, 3], # PC Leo
-                        #   device=0,  # Firebox laza
-                          # device=[1, 4], # PC laza
+                          # device=0,  # Firebox laza
                           # [s] tempo de gravação do ruído de fundo
                           noiseFloorTp=5,
                           # [s] tempo de gravação do sinal de calibração
@@ -67,14 +75,12 @@ MS = rmr.MeasurementSetup(name='med-teste',  # Nome da medição
                           outChannels={'O1': (1, 'Dodecaedro 1'),
                                        'O2': (2, 'Dodecaedro 2'),
                                        'O3': (3, 'Sistema da sala')})
-
-# %% Initializate Data object, wich manage the measurement files in dics
 D = rmr.MeasurementData(MS)
 
 # %% Mostra status da instância de dados medidos
 # D.getStatus()
 
-# %% Cria nova tomada de medição para uma nova configuração fonte receptor
+# %% Cria nova tomada de medição
 takeMeasure = rmr.TakeMeasure(MS=MS,
                               # Passa objeto de comunicação
                               # com o LabJack U3 + EI1050 probe
@@ -137,25 +143,32 @@ takeMeasure = rmr.TakeMeasure(MS=MS,
                               excitation='varredura',
                               # Código do canal de saída a ser utilizado.
                               outChSel='O1')
-# %% Acquire data
+# %% Inicia tomada de medição/aquisição de dados
 takeMeasure.run()
 
-# %% Salva tomada de medição no objeto de dados D e no disco
+# %% Salva tomada de medição no disco
 D.save_take(takeMeasure)
 
-# %% Carrega dados medidos e setup de medição do arquivo
-MS, D = rmr.med_load('med-teste')
-
-# %% Carrega coisas medidas de acordo com as tags fornecidas
+# %% Carrega um dicionário com MeasuredThings de acordo com as tags fornecidas
+# e faz algum processamento
 a = D.get('roomres', 'HATS')
+msdThing = a['roomres_S1-R1_O1-HATS_varredura_1']
+msdThing.measuredSignals[0].plot_time()
 
-# %%
-a = D.get('roomir')
+# %% Calcula respostas impulsivas aplicando calibrações e salva em disco (vide 
+# parâmetro skipSave)
+a = D.get('roomres')
+b = D.calculate_ir(a, calibrationTake=1, skipCalibration=False, skipSave=True)
+for IR in b.values():
+        IR.measuredSignals[0].plot_time()
 
-# %%
-b = D.calculate_ir(a, skipSave=False)
+# %% Formas alternativas de carregar dados na memória
+# %% Carrega MS e todas as MeasuredThings
+a = rmr.h5_load(MS.name + '/MeasurementData.hdf5')
 
-#%%
-
+# %% Carrega sinais de excitação utilizados
+a = rmr.h5_load(MS.name + '/MeasurementData.hdf5', skip=['MeasuredThing'])
+loadedExcitationSignals = a['MeasurementSetup'].excitationSignals
+loadedExcitationSignals['varredura'].plot_freq()
 
 #%%
