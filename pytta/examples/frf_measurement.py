c6a3f9a4436721fb317b2ee6b2ad145ead791bc4
Your name hereeric-brandao <ericbacustica@gmail.com>
2019-12-17 10:49:47 -0300

    [FIX] frf_measurement and roomac_testing examples

--- pytta/examples/frf_measurement.py
+++ pytta/examples/frf_measurement.py
@@ -12,11 +12,20 @@ sweep = pytta.generate.sweep(fftDegree=18)
 ms = pytta.generate.measurement('frf',
                                 excitation=sweep,
                                 samplingRate=44100,
-                                freqMin=20,
+                                freqMin=100,
                                 freqMax=20000,
-                                device=4,
-                                inChannel=[1],
-                                outChannel=[1, 2],
+                                device=13,
+                                inChannels=[1],
+                                outChannels=[1],
                                 comment='Testing; 1, 2.')
-
+#%%
 med1 = ms.run()
+
+#%%
+med1.plot_time()
+med1.plot_freq()
+
+#%%
+an = pytta.rooms.analyse(med1.IR, 'RT', 20, nthOct=3, minFreq=60, maxFreq=20000,
+                        plotLundebyResults=False, IRManualCut=1)
+
