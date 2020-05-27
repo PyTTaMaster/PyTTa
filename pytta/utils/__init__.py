431cae9560b6bacbd227d3cc9d88ed5ba2775e27
Chum4k3r <chum4k3r@github.com>
2020-05-27 15:24:51 -0300

    [INTERNAL] Moved frequtils to utils/freq

--- pytta/utils/__init__.py
+++ pytta/utils/__init__.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Utilities module.
+
+
 Created on Tue May  5 00:31:42 2020
 
-@author: joaovitor
+@author: João Vitor G. Paes
 """
 
 from .colore import ColorStr, colorir, pinta_texto, pinta_fundo
+from .freq import *
 from .maths import *
+
