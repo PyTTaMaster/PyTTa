da51ee13b1bdca1f233baf1a44420f5c08fa2284
Chum4k3r <chum4k3r@github.com>
2020-10-29 19:48:47 -0300

    [FIX] Remove fft_degree references from imported functions.

--- pytta/__init__.py
+++ pytta/__init__.py
@@ -6,7 +6,7 @@ and signal analysis.
 To get started, try:
 
     >>> import pytta
-    
+
     >>> pytta.default()
     >>> pytta.list_devices()
 
@@ -26,7 +26,7 @@ Our current dependencies are:
 PyTTa is now a multi-paradigm toolbox, which may change in the future. We aim
 from now on to be more Pythonic, and therefore more object-oriented. For now,
 we have classes and functions operating together to provide a working
-environment for acoustical measurements and post-processing. 
+environment for acoustical measurements and post-processing.
 
 The toolbox' structure is presented next.
 
@@ -42,17 +42,17 @@ Sub-packages:
         and also intended for new features whose should have an
         object-oriented implementation. The main classes are called from the
         toolbox's top-level (e.g. pytta.SignalObj, pytta.Analysis, ...);
-    
+
     * pytta.utils:
         contains simple tools which help to keep things modularized and make
         more accessible the reuse of some operations. Intended to hold tools
         (classes and functions) whose operate built-in python classes, NumPy
         arrays, and other stuff not contained by the pytta.classes subpackage;
-        
+
     * pytta.apps:
         applications built from the toolbox's functionalities. The apps
         are called from the top-level (e.g. pytta.roomir);
-        
+
 
 Modules:
 --------
@@ -60,18 +60,18 @@ Modules:
     * pytta.functions:
         assistant functions intended to manipulate and visualize multiple
         signals and analyses stored as SignalObj and Analysis objects. These
-        functions are called from the toolbox's top-level (e.g. 
+        functions are called from the toolbox's top-level (e.g.
         pytta.plot_time, pytta.plot_waterfall, ...);
-        
+
     * pytta.generate:
         functions for signal synthesis and measurement configuration;
-        
+
     * pytta.rooms:
         room acoustics parameters calculation according to ISO 3382-1;
-        
+
     * pytta.iso3741:
         calculations according to the standard;
-        
+
 
 You can find usage examples in the toolbox's examples folder.
 
@@ -112,7 +112,7 @@ from . import iso3741
 from .functions import read_wav, write_wav, merge, split, list_devices,\
     get_device_from_user,\
     fft_convolve, find_delay, corr_coef, resample,\
-    peak_time, save, load, fft_degree, plot_time, plot_time_dB, plot_freq,\
+    peak_time, save, load, plot_time, plot_time_dB, plot_freq,\
     plot_bars, plot_spectrogram, plot_waterfall
 
 from .apps import roomir
@@ -144,7 +144,6 @@ __all__ = [  # Apps
            'save',
            'load',
            'weighting',
-           'fft_degree',
            'plot_time',
            'plot_time_dB',
            'plot_freq',
