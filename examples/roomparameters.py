40a61b358e3c6fd9e99db1727c0b88ae07489e25
Chum4k3r <joaovitorgpaes@gmail.com>
2021-07-05 17:51:20 -0300

    [TESTS] Added some data for testing.

--- examples/roomparameters.py
+++ examples/roomparameters.py
@@ -20,12 +20,12 @@ room = pytta.RoomAnalysis(myMonoIR, nthOct=3, minFreq=50., maxFreq=16e3)
 print()
 print("Parameters from impulse response are:")
 print("\n", room.parameters, "\n")
-print("Access directly by RoomParameters().PNAME")
-print("Or view it in a bar plot by RoomParameters().plot_PNAME")
+print("Access directly by RoomAnalysis().PNAME")
+print("Or view it in a bar plot by RoomAnalysis().plot_PNAME")
 print("where PNAME is the name of the desired parameter, as shown above.")
 print()
 print(f"{room.T20=}")
 
 
 fig = room.plot_EDT()
-
+fig.show()
