After changing the DC-normalization in the _fft method, fix the backwards-transformation _ifft and _make_pk_spectra accordingly.
Since DC values are normalized with num_samples, we need to denormalize them accordingly in _ifft.
since no multiplication with 2**(1/2) is done for RMS of DC, we also do not need to divide this value in _make_pk_spectra

pytta/classes/signal.py
@@ -1132,8 +1132,13 @@ class SignalObj(_base.PyTTaObj):
         """
         # spectrum denormalization
         if self.signalType == 'power':
-            adjustedFreqSignal = \
-                self._freqSignal*len(self._freqSignal)
+            adjustedFreqSignal = np.zeros(self._freqSignal.shape, dtype=np.complex_)
+            # * N/2 for AC
+            adjustedFreqSignal[1:, :] = \
+                self._freqSignal * len(self._freqSignal)
+            # * N for DC
+            adjustedFreqSignal[0, :] = \
+                self._freqSignal[0, :] * self.numSamples
         else:
             adjustedFreqSignal = self._freqSignal
         # turning RMS amplitude into peak amplitude except DC freq
@@ -1636,7 +1641,7 @@ def _make_rms_spectra(freqSignal):
 
 def _make_pk_spectra(freqSignal):
     newFreqSignal = np.zeros(freqSignal.shape, dtype=np.complex_)
-    newFreqSignal[:,:] = freqSignal * 2**(1/2)
-    newFreqSignal[0,:] = freqSignal[0,:] / 2**(1/2)
+    newFreqSignal[1:, :] = freqSignal[1:, :] * 2**(1/2)
+    newFreqSignal[0, :] = freqSignal[0, :]
     return newFreqSignal
 